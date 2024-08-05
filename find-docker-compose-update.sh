0fe5aa65720eef7b437f0387d218b8334ea98de8
Grayson Stillwell <g.stillwell@stillautomation.com>
2024-08-06 11:47:22 +1200

    Updated find-docker-compose-update.sh and removed test log files

--- find-docker-compose-update.sh
+++ find-docker-compose-update.sh
@@ -26,12 +26,12 @@ for composes in $(sudo find $dir -type f -name $file)
 do echo "Pulling latest files for $composes"
     dir_path=$(dirname "$composes")
     
-    cd $dir_path && docker compose pull 1>> $logs/happy.txt 2>> $logs/not-happy.txt
-    echo "----updating:"$current"----" 1>> $logs/happy.txt && date 1>> $logs/happy.txt
+    cd $dir_path && docker compose pull 1>> $logs/docker-update-success.txt 2>> $logs/docker-update-error.txt
+    echo "----updating:"$current"----" 1>> $logs/docker-update-success.txt && date 1>> $logs/docker-update-success.txt
     current=$((current +1)) 
      
 
 done
 
 echo "-----"
-echo "End" 1>> $logs/happy.txt
+echo "End" 1>> $logs/docker-update-success.txt
