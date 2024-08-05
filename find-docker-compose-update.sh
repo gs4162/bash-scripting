ce5e49bf0b49b663b0bcab82dd47b43a149f0e8c
Grayson Stillwell <g.stillwell@stillautomation.com>
2024-08-05 14:24:33 +1200

    Added counter for each container

--- find-docker-compose-update.sh
+++ find-docker-compose-update.sh
@@ -12,9 +12,28 @@ then
     mkdir $logs
     chown $USER $logs
 fi
-sudo find $dir -type f -name $file
+
 
 #permission of logs to be owned by current user
 
 sudo chown $USER $logs
 sudo chmod +rw -R $logs 
+
+#update to latest images
+current=1
+
+for composes in $(sudo find $dir -type f -name $file)
+do echo "Pulling latest files for $composes"
+    dir_path=$(dirname "$composes")
+    
+    cd $dir_path && docker compose pull
+    echo "----updating:"$current"----"
+    current=$((current +1))
+     
+
+done
+
+echo "-----"
+echo "End"
+
+
