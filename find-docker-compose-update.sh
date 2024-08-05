8d2c1a0ab88064f1dcfd6fd2c8cf71de3228c517
Grayson Stillwell <g.stillwell@stillautomation.com>
2024-08-05 13:39:44 +1200

    permissions added, folder creation

--- find-docker-compose-update.sh
+++ find-docker-compose-update.sh
@@ -4,11 +4,17 @@ dir=/opt
 file="docker-compose.y*ml" #both yaml & yml
 logs=$(pwd)/logs
 
-#check log directory & permissions
+
+#check log directory, create & permissions
 
 if [ ! -d $logs ]
 then
-    sudo mkdir $logs
-    sudo chown $USER $logs
+    mkdir $logs
+    chown $USER $logs
 fi
 sudo find $dir -type f -name $file
+
+#permission of logs to be owned by current user
+
+sudo chown $USER $logs
+sudo chmod +rw -R $logs 
